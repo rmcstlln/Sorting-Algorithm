f5431e0a09d807ce8faad20412465540b01e8159
rmcstlln <romarkpinero68@gmail.com>
2023-01-24 22:44:33 +0800

    def insertionSort

--- Insertion.py
+++ Insertion.py
@@ -1,1 +1,10 @@
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while array[j - 1] > array[j] and j > 0:
+            array[j - 1], array[j] = array[j], array[j - 1]
+            j -= 1
+            print(array)
+
+
 array = [40, 97, 41, 3, 6, 79, 7, 62, 83, 91]
