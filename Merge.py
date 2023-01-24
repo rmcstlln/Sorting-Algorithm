30f71e717c4500f00a0fc9a8cc48d52a808d61a8
rmcstlln <romarkpinero68@gmail.com>
2023-01-24 22:53:43 +0800

    def mergeSort

--- Merge.py
+++ Merge.py
@@ -1,1 +1,35 @@
+def mergeSort(array):
+    if len(array) > 1:
+        left_array = array[:len(array) // 2]
+        right_array = array[len(array) // 2:]
+
+        # Recursion
+        mergeSort(left_array)
+        mergeSort(right_array)
+
+        # Merge
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                array[k] = left_array[i]
+                i += 1
+            else:
+                array[k] = right_array[j]
+                j += 1
+            k += 1
+
+        while i < len(left_array):
+            array[k] = left_array[i]
+            i += 1
+            k += 1
+
+        while j < len(right_array):
+            array[k] = right_array[j]
+            j += 1
+            k += 1
+
+
 array = [40, 97, 41, 3, 6, 79, 7, 62, 83, 91]
