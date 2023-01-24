22579e55561d4d0c3a5aedc4692aa589372c7229
rmcstlln <romarkpinero68@gmail.com>
2023-01-24 22:14:55 +0800

    def selectionSort

--- Selection.py
+++ Selection.py
@@ -1,1 +1,16 @@
+def selectionSort(array):
+
+    for i in range(9):
+        minpos = i
+        for j in range (i, 10):
+            if array[j] < array[minpos]:
+                minpos = j
+
+        temp = array[i]
+        array[i] = array[minpos]
+        array[minpos] = temp
+
+        print(array)
+
+
 array = [40, 97, 41, 3, 6, 79, 7, 62, 83, 91]
