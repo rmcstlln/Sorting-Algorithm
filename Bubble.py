f122aa4374579b84ceb35c08d40118fd20351e52
rmcstlln <romarkpinero68@gmail.com>
2023-01-24 22:26:16 +0800

    def bubbleSort

--- Bubble.py
+++ Bubble.py
@@ -1,1 +1,11 @@
+def bubbleSort(array):
+    for i in range(len(array) - 1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+                print(array)
+
+
 array = [40, 97, 41, 3, 6, 79, 7, 62, 83, 91]
